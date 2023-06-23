eae79c88eee164a087cfe1a7b5bcf284cdc5828d
Aditya Khandelwal <khandelwaladitya410@gmail.com>
2023-06-23 17:05:14 +0530

    Third Commit

--- new.js
+++ new.js
@@ -1,1 +1,13 @@
 console.log("Hello World")
+
+let array = [1,12,3,4,54,6]
+
+const newArray = array.map(multiplyByTwo);
+
+function multiplyByTwo(num){
+    return num * 2;
+}
+
+newArray.forEach((element)=>{
+    console.log(element);
+})
